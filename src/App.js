7023af1a93d9cefc2eab8351ca2e1e936a6168cb
Abhijeet Khire <abhijeet.khire@Eternus.local>
2020-03-28 01:53:55 +0530

    basic pwa changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state={
+      todos : null,
+      todoLoading : false,
+    }
+    
+  }
+  
+
+
+  getTodos = () => {
+
+    let url = "https://jsonplaceholder.typicode.com/todos";
+    this.setState({ todoLoading : true , todos : null});
+    
+     setTimeout(() => {
+      fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        data = data && data.slice(0, 10);
+        this.setState( { todos : data , todoLoading : false });
+      })
+     }, 2000);
+
+
+  }
+
+  render() {
+    return (
+      <div style={{ padding : "50px" }}>
+
+        <h1>PWA - reactjs </h1>
+
+        <button onClick={() => this.getTodos()}>Get Todos</button>
+
+        {this.state.todoLoading && <h2>Loading.... todo's API </h2>}
+
+
+        {
+          this.state.todos && this.state.todos.map( (e , i)=>{
+          return <h4 key={i}>{i+1}. {e.title}</h4>
+          })
+        }
+
+      </div>
+    );
+  }
 }
 
 export default App;
